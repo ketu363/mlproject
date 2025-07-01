8253e1c0a1e5b1aa8bdca893b14a54d8d7031fe5
ketu363 <nishantketu363@gmail.com>
2025-07-01 19:29:05 +0530

    Data transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,11 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+# This is data ingestion class so if any data requried we can give this class
+# There is decorator @data class by use of it we can directly define our class variable.
 @dataclass
 class DataIngectionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -51,7 +55,11 @@ class DataIngestion:
 # initiate it for running 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
 
 
 
