2b66cec0680de60a110adb0440499466126a5997
ketu363 <nishantketu363@gmail.com>
2025-07-02 14:13:19 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,21 +22,31 @@ def save_object(file_path,obj):
         raise CustomException(e, sys)        
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             # Taking the all madels one by on in model variable
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            # Applying greed search cv
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            # Setting the best parameter to the model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             # Fitiing the model
-            model.fit(X_train, y_train)
+            #model.fit(X_train, y_train)
 
             # Doing prediction on X_train and y_train data
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
+
             # r2 score evaluation on the training model
             train_model_score = r2_score(y_train, y_train_pred)
 
