b4a1308632c0c50aadb3a9b8d956b8566b76e785
ketu363 <nishantketu363@gmail.com>
2025-07-03 19:31:15 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -61,3 +61,16 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         
     except Exception as e:
         raise CustomException(e,sys)   
+    
+
+def load_object(file_path):
+        
+        """
+        Function to load an object from a file using dill.
+        """
+
+        try:
+            with open(file_path, "rb") as file_obj:
+                return dill.load(file_obj)
+        except Exception as e:
+            raise CustomException(e, sys)    
